8829bbd1b727561642b743a7721edf35f9a6b269
Rahul Kumar Yadav <131392643+Yaduvanshirahul@users.noreply.github.com>
2025-11-19 11:33:52 +0530

    Learn about the prototype

---
+++ 6_Javascript/13_OOPS/oops5.js
@@ -0,0 +1,15 @@
+Function.prototype.mybind = function(){
+    console.log("rahul");
+    
+}
+
+function fun(){
+    console.log("ram");
+    
+}
+function fun1(){
+    console.log("raj");
+    
+}
+
+// here you know you can access function mybind because we make the mybind function prototype as a function which is accessble by any function
--- 6_Javascript/1_DataTypes/2_hoisting.js
+++ 6_Javascript/1_DataTypes/2_hoisting.js
@@ -10,15 +10,26 @@ var a;
 console.log(a); // here 'undefined' value of a is print in other language it show error
 
 // var a; --> these line moves to up when variable declare after console. that's how hoisting work.
-
-
+fun() // here it execute as it is function
+// fun1() // it is not a function it is a variable
 a =12;
 
 console.log(b)
 var b;
+
+function fun(){
+    console.log("hello"
+    );
+    
+}
+
+var fun1 = () =>{
+    console.log("hello1");
+    
+} // 
 /* 
 there is difference between undefined and not-defined 
    
 undefined --> which is exist but unknown.
 not-defined --> which is not exist.
-*/
+*/ 
